e8d7846c1d81d534b3f7d1c59ec2819e0b78fd65
KejviBode <trainee.kejvi.bode@sigmalabs.co.uk>
2023-05-16 17:54:40 +0100

    Start adding functionality to matching tiktok and spotify songs

--- extract_spotify/extract.py
+++ extract_spotify/extract.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import spacy
+from rapidfuzz import fuzz, distance
+from rapidfuzz.distance import Levenshtein
+
+nlp = spacy.load("en_core_web_sm")
 
 BASE_URL = "https://ads.tiktok.com/business/creativecenter/inspiration/popular/music/pad/en"
 TIKTOK_COOKIE = {"name": "cookie-consent", "value": "{%22ga%22:true%2C%22af%22:true%2C%22fbp%22:true%2C%22lip%22:true%2C%22bing%22:true%2C%22ttads%22:true%2C%22reddit%22:true%2C%22criteo%22:true%2C%22version%22:%22v9%22}"}
@@ -46,12 +50,26 @@ def scrape_tiktok_soup(soup: BeautifulSoup) -> list[dict]:
         song_info["song_name"] = song_name
         song_info["song_position"] = song_position
         song_info["artists"] = song_artists
+        song_info["on_tiktok"] = True
+        song_info["on_spotify"] = False
         song_data.append(song_info)
     return song_data
 
+def match_tiktok_to_spotify(tiktok_tracks: list[dict], spotify_tracks: list[dict]):
+    tiktok_not_on_spotify_chart = []
+    for tiktok_track in tiktok_tracks:
+        for spotify_track in spotify_tracks:
+            if tiktok_track["song_name"] == spotify_track["name"]:
+                spotify_track["on_tiktok"] = True
+            # if tiktok_track
+
 def handler():
+    print("Fetching html from TikTok charts...")
     soup = load_tiktok_html_soup(BASE_URL)
+    print("Complete!")
+    print("Scraping data from TikTok html...")
     songs = scrape_tiktok_soup(soup)
+    print("Complete!")
     print(songs)
 
 if __name__ == "__main__":
