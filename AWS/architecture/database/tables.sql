a55c07a977045378fb4483590c81cf2f77812e0c
Coderilz <116507723+Coderilz@users.noreply.github.com>
2023-05-17 14:42:40 +0100

    using spotify_id for tracks and artists

--- AWS/architecture/database/tables.sql
+++ AWS/architecture/database/tables.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS genre;
 DROP TABLE IF EXISTS track;
 
 CREATE TABLE IF NOT EXISTS track (
-    track_id INT GENERATED ALWAYS AS IDENTITY,
+    track_spotify_id VARCHAR(200) NOT NULL,
     track_name VARCHAR(200) NOT NULL,
     track_danceability FLOAT NOT NULL,
     track_energy FLOAT NOT NULL,
@@ -18,26 +18,24 @@ CREATE TABLE IF NOT EXISTS track (
     in_tiktok BOOLEAN NOT NULL DEFAULT false,
     tiktok_rank INT,
     spotify_rank INT,
-    track_spotify_id VARCHAR(200),
     created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-    PRIMARY KEY(track_id)
+    PRIMARY KEY(track_spotify_id)
 );
 
 CREATE TABLE IF NOT EXISTS artist (
-    artist_id INT GENERATED ALWAYS AS IDENTITY,
-    spotify_name VARCHAR(100),
     artist_spotify_id VARCHAR(200) NOT NULL,
-    PRIMARY KEY(artist_id)
+    spotify_name VARCHAR(100),
+    PRIMARY KEY(artist_spotify_id)
 );
 
 CREATE TABLE IF NOT EXISTS artist_popularity (
     popularity_id INT GENERATED ALWAYS AS IDENTITY,
-    artist_id INT NOT NULL,
+    artist_spotify_id VARCHAR(200) NOT NULL,
     artist_popularity INT NOT NULL,
     follower_count INT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
     PRIMARY KEY(popularity_id),
-    FOREIGN KEY(artist_id) REFERENCES artist(artist_id)
+    FOREIGN KEY(artist_spotify_id) REFERENCES artist(artist_spotify_id)
 );
 
 CREATE TABLE IF NOT EXISTS genre (
@@ -48,27 +46,27 @@ CREATE TABLE IF NOT EXISTS genre (
 
 CREATE TABLE IF NOT EXISTS artist_genre (
     artist_genre_id INT GENERATED ALWAYS AS IDENTITY,
-    artist_id INT NOT NULL,
+    artist_spotify_id VARCHAR(200) NOT NULL,
     genre_id INT NOT NULL,
     PRIMARY KEY(artist_genre_id),
-    FOREIGN KEY(artist_id) REFERENCES artist(artist_id),
+    FOREIGN KEY(artist_spotify_id) REFERENCES artist(artist_spotify_id),
     FOREIGN KEY(genre_id) REFERENCES genre(genre_id)
 );
 
 CREATE TABLE IF NOT EXISTS track_popularity (
     track_popularity_id INT GENERATED ALWAYS AS IDENTITY,
-    track_id INT NOT NULL,
+    track_spotify_id VARCHAR(200) NOT NULL,
     popularity_score INT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
     PRIMARY KEY(track_popularity_id),
-    FOREIGN KEY(track_id) REFERENCES track(track_id)
+    FOREIGN KEY(track_spotify_id) REFERENCES track(track_spotify_id)
 );
 
 CREATE TABLE IF NOT EXISTS track_artist (
     track_artist_id INT GENERATED ALWAYS AS IDENTITY,
-    track_id INT NOT NULL,
-    artist_id INT NOT NULL,
+    track_spotify_id VARCHAR(200) NOT NULL,
+    artist_spotify_id VARCHAR(200) NOT NULL,
     PRIMARY KEY(track_artist_id),
-    FOREIGN KEY(track_id) REFERENCES track(track_id),
-    FOREIGN KEY(artist_id) REFERENCES artist(artist_id)
+    FOREIGN KEY(track_spotify_id) REFERENCES track(track_spotify_id),
+    FOREIGN KEY(artist_spotify_id) REFERENCES artist(artist_spotify_id)
 );
