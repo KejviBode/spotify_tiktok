739510900a05c9e5d1ae2b1d26fb8cb43dfba953
KejviBode <trainee.kejvi.bode@sigmalabs.co.uk>
2023-05-19 09:38:46 +0100

    Save current working version before adapting

--- extract/extract_tiktok.py
+++ extract/extract_tiktok.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver import Firefox, FirefoxProfile
 from bs4 import BeautifulSoup
 from rapidfuzz import fuzz
 from datetime import datetime
